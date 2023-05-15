a062da78fe7a92db36aad9d6f2cebbb69fd49ae2
frazbakht5 <mr.fraz.bakht@gmail.com>
2023-05-15 12:01:06 +0500

    added joi req body validation

--- node-js/index.js
+++ node-js/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 var bodyParser = require('body-parser'); // npm i body-parser
 const app = express()
+const Joi = require('joi');
 
 
 /* app.get(route, func);
@@ -44,6 +45,54 @@ app.post('/car', async function (req, res) {
 	}
 	res.send(resObject)
 });
+
+
+const handler = (req, res) => {
+
+	let bodyParams = req.body;
+
+	console.log("Entrying following data for user in db:");
+	console.log(JSON.stringify(bodyParams));
+
+	console.log(bodyParams);
+
+	let resObject = {
+		status: 201,
+		data: "New user added in db"
+	}
+	res.send(resObject)
+}
+
+const userMiddleware = (req, res, next) => {
+
+	const schema = Joi.object().keys({
+		username: Joi.string().required(),
+		password: Joi.string().min(4).max(15).required(),
+		gender: Joi.boolean().required(),
+		age: Joi.number()
+	});
+
+	const { value, error } = Joi.compile(schema)
+		.prefs({ errors: { label: 'key' }, abortEarly: false })
+		.validate(req.body);
+
+	const valid = error == null;
+
+	console.log("valid? ===> ", valid);
+
+	if (!valid) {
+		console.log("Validation failed");
+		let resObject = {
+			status: 400,
+			msg: "Validation failed"
+		}
+		res.send(resObject)
+	} else {
+		next()
+	}
+}
+
+app.post('/user', userMiddleware, handler);
 
 app.listen(5000, () => {
 	console.log(`Techlift app listening on port ${5000}`)
